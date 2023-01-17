418b74934d5eb4fbb584d3dca7a729552fe75c71
Christopher <christopherschmitz96@gmail.com>
2023-01-17 18:27:50 +0100

    Replace Strings with EndpointType

--- src/ts/class/Swapi.tsx
+++ src/ts/class/Swapi.tsx
@@ -1,27 +1,29 @@
 const axios = require('axios').default;
 
+type EndpointType = 'people';
+
 class SwapiClass {
     baseUrl: string;
+    readonly PeopleEndpoint = 'people';
 
     constructor() {
         this.baseUrl = "https://swapi.dev/api/";
     }
 
-    //TODO Replace String with Type Endpoint here
-    getEndpointUrl(endpoint: string) {
+    getEndpointUrl(endpoint: EndpointType, searchString?: string) {
         switch(endpoint) { 
-            case 'people':{
-                return `${this.baseUrl}people`;
+            case this.PeopleEndpoint:{
+                return `${this.baseUrl}${this.PeopleEndpoint}${(searchString) ? '/?search='+searchString : ''}`;
             }
         };
     }
 
     async getPeopleById(id : number) {
-        return axios.get(`${this.getEndpointUrl('people')}/${id}`);
+        return axios.get(`${this.getEndpointUrl(this.PeopleEndpoint)}/${id}`);
     }
 
     async searchPeople(searchString: string) {
-        return axios.get(`${this.getEndpointUrl('people')}/?search=${searchString}`);
+        return axios.get(`${this.getEndpointUrl(this.PeopleEndpoint, searchString)}`);
     }
 }
 
